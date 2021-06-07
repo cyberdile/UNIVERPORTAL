b1c745a6f9a658ba7ba5946118910af6a3f1e693
Артемьева Александра Борисовна <abartem1@mts.ru>
2021-06-08 00:57:58 +0300

    changed buttons and posts

--- anothertry/urls.py
+++ anothertry/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from .yasg import urlpatterns as doc_urls
+#from .yasg import urlpatterns as doc_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,4 +23,4 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
 ]
 
-urlpatterns += doc_urls
+#urlpatterns += doc_urls
